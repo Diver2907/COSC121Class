0669ef6668b0734689caa61a92fc0d8790398e78
Cooper Ross <c.mross2907@gmail.com>
2023-02-13 09:45:23 -0800

    Made recursive function

---
+++ Recursion/Recursion.java
@@ -0,0 +1,14 @@
+package Recursion;
+
+public class main{
+    public static void main(String[] args){
+        Print(5);
+    }
+    void Print(int n){
+        if(n>0){
+            System.out.print(n+" ");
+            print(n-1);
+        }
+        return;
+    }
+}
